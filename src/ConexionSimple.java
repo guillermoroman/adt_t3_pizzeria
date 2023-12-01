b8f3ef6fabebd9fd2329bc1dd433f07a1e377aa3
guillermoroman <guillermoroman@outlook.com>
2023-12-01 09:11:52 +0100

    Create imprimirPizzas & imprimirMenu

--- src/ConexionSimple.java
+++ src/ConexionSimple.java
@@ -1,10 +1,12 @@
+import javax.xml.transform.Result;
 import java.sql.*;
 
 public class ConexionSimple {
 
     public static void main(String[] args) {
-        pruebaSencilla();
-        //prueba2();
+        //pruebaSencilla();
+        //imprimirPizzas();
+        imprimirMenu();
     }
     public static void pruebaSencilla(){
 
@@ -23,10 +25,32 @@ public class ConexionSimple {
             System.out.println("Error al conectar a la base de datos.");
         }
     }
-    public static void prueba2(){
-        // URL de conexión a la base de datos
+
+    public static void imprimirPizzas(){
         String url = "jdbc:mariadb://localhost:3306/adt_t3";
-        // Nombre de usuario y contraseña
+        String usuario = "root";
+        String contrasena = "";
+
+        // Establecer la conexión
+        try {
+            Connection connection = DriverManager.getConnection(url, usuario, contrasena);
+            Statement statement =  connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM pizzas");
+
+            while (resultSet.next()){
+                String nombre = resultSet.getString("nombre");
+                System.out.println(nombre);
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error al conectar a la base de datos.");
+        }
+    }
+
+    public static void imprimirMenu(){
+        String url = "jdbc:mariadb://localhost:3306/adt_t3";
         String usuario = "root";
         String contrasena = "";
 
@@ -34,17 +58,34 @@ public class ConexionSimple {
         try {
             Connection connection = DriverManager.getConnection(url, usuario, contrasena);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM pizzas");
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
+
+            ResultSet resultSet = statement.executeQuery("SELECT nombre, precio, ingredientes FROM pizzas");
+
+            while (resultSet.next()){
                 String nombre = resultSet.getString("nombre");
-                System.out.println(nombre);
+                Double precio = resultSet.getDouble("precio");
+                String ingredientes = resultSet.getString("ingredientes");
+
+                System.out.println(nombre + "........"+ precio);
+                System.out.println(ingredientes);
+                System.out.println();
             }
-            statement.close();
             connection.close();
-        } catch (SQLException e) {
+
+        } catch (SQLException e){
             e.printStackTrace();
             System.out.println("Error al conectar a la base de datos.");
         }
     }
 }
+
+//Listar todas las pizzas en formato menu.
+/*
+[Nombre de pizza]......[precio]
+[Ingredientes]
+
+[Nombre de pizza]
+
+
+
+ */
